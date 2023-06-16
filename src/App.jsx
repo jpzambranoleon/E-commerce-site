d5cf1f9835cff4b051382e320d294e2ee761566a
Jean-Paul <jpzambrano.leon@gmail.com>
2023-06-16 00:18:29 -0500

    modified file

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductPage, { CartContext } from "./pages/ProductPage";
+import { useState, useEffect } from "react";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [cartItem, setCartItem] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItem([...cartItem, item]);
+  };
+
+  // local storage
+  useEffect(() => {
+    const json = localStorage.getItem("cartItem");
+    const savedCart = JSON.parse(json);
+    if (savedCart) {
+      setCartItem(savedCart);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(cartItem);
+    localStorage.setItem("cartItem", json);
+  }, [cartItem]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <CartContext.Provider value={{ cartItem, addToCart, setCartItem }}>
+          <Navbar />
+          <Routes>
+            <Route index path="/" element={<Home />} />
+          </Routes>
+        </CartContext.Provider>
+      </Router>
     </>
   );
 }
